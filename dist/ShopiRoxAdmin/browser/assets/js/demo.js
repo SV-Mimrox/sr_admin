0a1c1affb5f48e796402bf253651f401f67ce194
SV-Mimrox <sv.mimrox@gmail.com>
2024-12-05 09:01:01 +0530

    shopirox admin

---
+++ dist/ShopiRoxAdmin/browser/assets/js/demo.js
@@ -0,0 +1,12 @@
+
+
+// function mydemo(){
+//     alert("test");
+//     $(".metaInfo").blur(function(){
+//         var txt = $(this).val();
+//         $(".metaTitleText").val(txt);
+//         $(".metaDescText").val(txt);
+//       });
+// }
+
+       
